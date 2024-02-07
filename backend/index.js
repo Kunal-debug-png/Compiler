2e12e6fc295d22fdb09e151c3941ca25e6dbea5e
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-02-07 23:57:17 +0530

    made a call to executeJava function

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,22 @@
 const express= require("express");
 const app=express();
+const { generateFile}=require('./generateFile');
+const {executeJava} = require("./executeJava");
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.get('/',(req,res)=>{
    return  res.json({hello:"world:"});
 });
-app.post("/run",(req,res) => {
+app.post("/run",async(req,res) => {
     const {language = "cpp",code} = req.body;
     if(code===undefined){
         return res.status(400).json({ success:false,error: "Empty code body"});
     }
     //need to generate a c++ file with content from the request
+    const filepath=await generateFile(language,code);
+    const output = await executeJava(filepath);
     //we need to run file and send the response
-    return res.json({language,code});
+    return res.json({filepath,output});
 })
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
